88e26e91848ca76652cfe3d7d50b3065ed1f4f15
AnhTT <kimcuongden1712@gmail.com>
2022-09-11 13:16:55 +0700

    1.9: Triển khai Logger Service với Serilog

--- src/BuildingBlocks/Common.Logging/Serilogger.cs
+++ src/BuildingBlocks/Common.Logging/Serilogger.cs
@@ -1,7 +1,24 @@
-﻿namespace Common.Logging
-{
-    public class Serilogger
-    {
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
 
+namespace Common.Logging
+{
+    public static class Serilogger
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
+            {
+                var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-");
+                var enviromentName = context.HostingEnvironment.EnvironmentName ?? "Devlopment";
+
+                configuration
+                .WriteTo.Debug()
+                .WriteTo.Console(outputTemplate:
+                "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Environment", enviromentName)
+                .Enrich.WithProperty("Application", applicationName)
+                .ReadFrom.Configuration(context.Configuration);
+            };
     }
 }
