183f29e961406ce61d4c668d1e8350c6b94da2be
AnhTT <kimcuongden1712@gmail.com>
2022-09-17 23:48:05 +0700

    3.2: Khởi tạo Project Web API và các Entity, Dtos cho Customer.API Microservices

---
+++ src/Services/Customer.API/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using Contracts.Domains.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product.API.Entities
+{
+    public class Customer : EntityAuditBase<int>
+    {
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
